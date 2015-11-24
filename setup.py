cf666821462ceba60c9bb1cb57d32a8d935d2d79
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-11-24 19:17:53 +0100

    made basic setup.py structure

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-__author__ = 'nickdg'
+from os import path
+from setuptools import setup
+
+script_dir = 'scripts'
+
+setup(name='c3d_repair',
+      version='0.2',
+      description='Scripts for rewriting c3d files to be compatible with various acquisition and analysis systems.',
+      scripts=[path.join(script_dir, 'convert_c3d.py')],
+      requires=['btk']
+      )
+
