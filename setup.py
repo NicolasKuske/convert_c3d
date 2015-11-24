acbc56da32603e62dab36fa95609ee7830329ee5
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-11-24 19:20:58 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 script_dir = 'scripts'
 
-setup(name='c3d_repair',
+setup(name='convert_c3d',
       version='0.2',
       description='Scripts for rewriting c3d files to be compatible with various acquisition and analysis systems.',
       scripts=[path.join(script_dir, 'convert_c3d.py')],
