47534abbc1ffac89fd2832ebdf2c71643790a4a2
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-11-25 19:28:14 +0100

    some more refactoring--needs testing, but should work!

--- scripts/convert_c3d.py
+++ scripts/convert_c3d.py
@@ -5,8 +5,6 @@ import Tkinter, tkFileDialog
 import os
 import argparse
 
-
-
 def convert_and_write(from_c3d_filename, to_c3d_filename):
 
     # Makes reader from filename
@@ -29,69 +27,65 @@ def convert_and_write(from_c3d_filename, to_c3d_filename):
     writer.Update()
 
 
-# TODO: This series of 'if' statements is too complex.  Please simplify: Get Filenames-->Check Directories-->Read-->Convert-->Write
-
-if args.c3d_file and not args.c3d_folder:
-    convert_and_write(args.c3d_file)
-
-elif args.c3d_folder and not args.c3d_file:
-    for c3d_file in os.listdir(args.c3d_folder):
-        if c3d_file.endswith(".c3d"):
-            convert_and_write(''.join([args.c3d_folder,"\\"+c3d_file]))
-
-elif args.c3d_folder and args.c3d_file:
-    raise Exception("Convert a file OR a folder")
-
-else:
-    root = Tkinter.Tk()
-
-    def write_c3d_file():
+def gui_load_c3d_filenames():
+        root = Tkinter.Tk()
         root.withdraw()
-        file_u=tkFileDialog.askopenfilename(title='Choose a c3d file to convert: ', filetypes=[('motive c3d tracking files', '*.c3d')])
-        convert_and_write(file_u.encode("ascii"))
+        c3d_filenames = tkFileDialog.askopenfilename(title='Choose a c3d file to convert: ',
+                                                filetypes=[('motive c3d tracking files', '*.c3d')],
+                                                multiple=True).encode("ascii")
         root.quit()
+        return c3d_filenames
 
-    def write_c3d_folder():
+
+def gui_save_c3d_directory():
+        root = Tkinter.Tk()
         root.withdraw()
-        c3d_folder=tkFileDialog.askdirectory(title='Choose a folder with c3d files to convert:').encode("ascii")
-        for c3d_file in os.listdir(c3d_folder):
-            if c3d_file.endswith(".c3d"):
-             convert_and_write(''.join([c3d_folder,"\\"+c3d_file]))
+        save_dir = tkFileDialog.askdirectory(title='Choose a directory to save to: ')
         root.quit()
-
-    frame=Tkinter.Frame(root)
-    frame.pack()
-
-    quit_button=Tkinter.Button(frame, text="QUIT", fg="red", command=frame.quit)
-    quit_button.pack(side=Tkinter.LEFT)
-
-    c3d_file_button = Tkinter.Button(frame, text="c3d file to convert", command=write_c3d_file)
-    c3d_file_button.pack(side=Tkinter.LEFT)
-
-    c3d_folder_button = Tkinter.Button(frame, text="folder with c3d files to convert", command=write_c3d_folder)
-    c3d_folder_button.pack(side=Tkinter.LEFT)
+        return save_dir
 
 
-    Tkinter.mainloop()
-
-
-
-#
 if __name__ == '__main__':
 
     # Parse Command-Line Arguments
     parser = argparse.ArgumentParser(description="This script converts a c3d file as exported by motive to a vicon readable c3d file.",
                                  epilog="If no arguments are given, the script first opens a window to let you choose a c3d file or a directory of c3d files to convert. \n")
 
-    parser.add_argument('-f', action='store', dest='c3d_file', default='', help='Name of the c3d file to convert.')
+    parser.add_argument('-f', action='store', dest='c3d_file', default='',
+                        help='Name of the c3d file to convert.')
 
-    parser.add_argument('-F', action='store', dest='c3d_folder', default='', help='Name of the folder which contains c3d files to convert.')
+    #parser.add_argument('-F', action='store', dest='c3d_folder', default='', help='Name of the folder which contains c3d files to convert.')
 
-    parser.add_argument('-s', action='store', dest='write_c3d_folder', default='jifoew', help='Folder where converted c3d files will be written to.')
+    parser.add_argument('-s', action='store', dest='write_c3d_folder', default='jifoew',
+                        help='Folder where converted c3d files will be written to.')
+
+    parser.add_argment('--append', action='store', dest='save_filename_append', default='_vicon',
+                       help='string to append to new files')
 
     args = parser.parse_args()
 
-    #
+    # Make sure only a file or folder (not both) is specified)
+    if args.c3d_folder and args.c3d_file:
+        raise Exception("Convert a file OR a folder")
+
+    # Get Filenames
+    filenames = [args.c3d_file] if args.c3d_file  else gui_load_c3d_filenames()
+
+    # Get Destination Directory (Save Directory)
+    if args.write_c3d_folder:
+        # Check to make sure it exists, and if not, make one.
+        save_dir = args.write_c3d_folder
+    else:
+        save_dir = gui_save_c3d_directory()
+
+    # Convert and Write to Each File
+    for name in filenames:
+        load_dir, base_name = os.path.split(name)
+        _, ext = os.path.splitext(name)
+        convert_and_write(name, os.path.join(save_dir, base_name+args.save_filename_append+ext))
+
+
+
 
 
 
