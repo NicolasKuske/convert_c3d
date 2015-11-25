a14c55cc34116598bc81af3c4858c1961b59b68a
unknown <nico.kuske@hotmail.de>
2015-11-25 16:55:38 +0100

    commit to merge

--- build/scripts-2.7/convert_c3d.py
+++ build/scripts-2.7/convert_c3d.py
@@ -1,14 +1,14 @@
 __author__ = 'Nico'
 
 import btk
-import Tkinter, tkFileDialog
+import Tkinter, tkFileDialog, Tkconstants
+from Tkinter import *
+import tkFileDialog
 import os
 import argparse
 
 
 def convert_and_write(c3d_file):
-    directory, extension = os.path.splitext(c3d_file)
-    converted_c3d_file= ''.join([directory, '_vicon', extension])
 
     reader = btk.btkAcquisitionFileReader()
     reader.SetFilename(c3d_file)
@@ -23,10 +23,32 @@ def convert_and_write(c3d_file):
             data=point.GetValues()[f,:]
             point.SetDataSlice(f, *[1000*x for x in data])
 
+    directory, extension = os.path.splitext(c3d_file)
+    converted_c3d_file= ''.join([directory, '_vicon', extension])
     writer = btk.btkAcquisitionFileWriter()
     writer.SetInput(acq)
     writer.SetFilename(converted_c3d_file)
     writer.Update()
+
+class App:
+  def __init__(self, master):
+    frame = Frame(master)
+    frame.pack()
+    self.button = Button(frame,
+                         text="QUIT", fg="red",
+                         command=frame.quit)
+    self.button.pack(side=LEFT)
+    self.slogan = Button(frame,
+                         text="Hello",
+                         command=self.write_slogan)
+    self.slogan.pack(side=LEFT)
+  def write_slogan(self):
+    root.withdraw()
+    c3d_folder=tkFileDialog.askdirectory(title='Choose a folder with c3d files to convert').encode("ascii")
+    for c3d_file in os.listdir(c3d_folder):
+         if c3d_file.endswith(".c3d"):
+             convert_and_write(''.join([c3d_folder,"\\"+c3d_file]))
+    root.quit()
 
 
 parser = argparse.ArgumentParser(description="This script converts a c3d file as exported by motive to a vicon readable c3d file.",
@@ -47,23 +69,42 @@ if args.c3d_file and not args.c3d_folder:
 elif args.c3d_folder and not args.c3d_file:
     for c3d_file in os.listdir(args.c3d_folder):
         if c3d_file.endswith(".c3d"):
-               convert_and_write(''.join([args.c3d_folder,"\\"+c3d_file]))
+            convert_and_write(''.join([args.c3d_folder,"\\"+c3d_file]))
 
 elif args.c3d_folder and args.c3d_file:
     raise Exception("Convert a file OR a folder")
 
 else:
-    root = Tkinter.Tk()
-    root.withdraw()
+
+
+    #root = Tkinter.Tk()
+    #root.withdraw()
+
+
+    # master = Tkinter.Tk()
+    #
+    # def callback():
+    #     print "click!"
+    #
+    # b = Tkinter.Button(master, text="OK", command=callback)
+    # b.pack()
+    # Tkinter.mainloop()
+
+
+
+    root = Tk()
+    app = App(root)
+    root.mainloop()
+
+    #button_opt = {'fill': Tkconstants.BOTH, 'padx': 5, 'pady': 5}
+    #Tkinter.Button(text='askopenfilename', command=askdirectory).pack(**button_opt)
     #file_u=tkFileDialog.askopenfilename(title='Choose a c3d file to convert: ', filetypes=[('motive c3d tracking files', '*.c3d')])
     #convert_and_write(file_u.encode("ascii"))
-    c3d_folder_u=tkFileDialog.askdirectory(title='Choose a folder with c3d files to convert') #get directory
-    c3d_folder=c3d_folder_u.encode("ascii")
-    print c3d_folder_u.encode("ascii")
-    print c3d_folder_u.encode("ascii")
-    print c3d_folder_u.encode("ascii")
-    print c3d_folder_u.encode("ascii")
-    for c3d_file in os.listdir(c3d_folder):
-        if c3d_file.endswith(".c3d"):
-            convert_and_write(''.join([c3d_folder,"\\"+c3d_file]))
+    #def askdirectory:
+
+    # c3d_folder_u=tkFileDialog.askdirectory(title='Choose a folder with c3d files to convert') #get directory
+    # c3d_folder=c3d_folder_u.encode("ascii")
+    # for c3d_file in os.listdir(c3d_folder):
+    #     if c3d_file.endswith(".c3d"):
+    #         convert_and_write(''.join([c3d_folder,"\\"+c3d_file]))
 
